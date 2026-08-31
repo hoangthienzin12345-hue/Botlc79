0dcf0118d1f7d25f40d3cc44791379111b97a659
hoangthienzin12345-hue <hoangthienzin12345@gmail.com>
2026-08-31 22:18:05 +0700

    bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ const logger = {
 // ==========================================
 // CẤU HÌNH BOT
 // ==========================================
-const BOT_TOKEN = '8812676489:AAFWTJS8dpvf4nwe2rFYywIkMIZgWw2IaTU';
+const BOT_TOKEN = '8574423779:AAECp-4sMKuj6VKWtF56XKUw6TpGl7Qice0';
 const ADMIN_ID = 8226483750;
 const ADMIN_USERNAME = "@ChAnTaoDz";
 
